97e3afa3b8642654db0106107b1d0fba1598294c
Vitaly Senokosov <6545415@gmail.com>
2025-08-24 19:06:45 +0300

    add todo-list component

--- todo-list/src/app/app.ts
+++ todo-list/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TodoList } from "../components/todo-list/todo-list";
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, TodoList],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
