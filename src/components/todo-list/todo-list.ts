d9fa703c71a174424e8cbd2084f9162d752fe44f
Vitaly Senokosov <6545415@gmail.com>
2025-09-07 17:34:52 +0300

    add and use Angular Material

--- src/components/todo-list/todo-list.ts
+++ src/components/todo-list/todo-list.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { TodoItem } from '../todo-item/todo-item';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css',
 })
