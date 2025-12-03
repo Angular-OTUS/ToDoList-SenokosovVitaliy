7062869e5807957a3acece7b12f5158616e41e1f
Vitaly Senokosov <6545415@gmail.com>
2025-12-03 23:09:59 +0300

    update styling, add header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TodoList } from "../components/todo-list/todo-list";
+import { Header } from "../components/header/header";
 
 @Component({
-  imports: [RouterModule, TodoList],
+  imports: [RouterModule, TodoList, Header],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
