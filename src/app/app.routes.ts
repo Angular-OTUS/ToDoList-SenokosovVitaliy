4170defdce1746ab283d2e98b5ef719e2824abb5
Vitaly <6545415@gmail.com>
2026-03-16 10:50:30 +0300

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { TodoList } from '../components/todo-list/todo-list';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: 'tasks', component: TodoList },
+    { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+    { path: '**', redirectTo: 'tasks' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TodoList } from "../components/todo-list/todo-list";
 import { Header } from "../components/header/header";
 import { User as User } from "../components/user/user";
 import { Toasts } from "../components/toasts/toasts.component";
 
 @Component({
-  imports: [RouterModule, TodoList, Header, User, Toasts],
+  imports: [RouterModule, Header, User, Toasts],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
